8e7ac85e557d9834b718eb71575d7bdb6aa8f7d8
root <kashmiranagwekar@gmail.com>
2020-10-27 22:47:14 +0530

    More logs in finish JS

--- amd/src/etconnect.js
+++ amd/src/etconnect.js
@@ -15,6 +15,7 @@ export const start = (quizname, fullname, idnumber, proctorid, quizid, username,
 };
 
 export const finish = (first, last) => {
+    window.console.log(`Test msg`);
     window.console.log(`The user name is '${first}' and the id is '${last}'`);
     if (window.location !== window.parent.location) {
         window.parent.postMessage({
